5d4d3a4e46e8c5e92568d5e930b0172fca64ff6d
tmucato <tomasmucato@gmail.com>
2022-05-19 13:41:19 +0200

    styling footer and header

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    date: Date = new Date();
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
