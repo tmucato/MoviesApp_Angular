16706a7cc2329d9bdec69265bc4cfdd010e582ac
tmucato <tomasmucato@gmail.com>
2022-10-18 16:47:53 +0200

    case treiler source remove object calling from component

--- src/app/components/video-embed/video-embed.component.ts
+++ src/app/components/video-embed/video-embed.component.ts
@@ -1,22 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
-  selector: 'video-embed',
-  templateUrl: './video-embed.component.html',
-  styleUrls: ['./video-embed.component.scss']
+    selector: 'video-embed',
+    templateUrl: './video-embed.component.html',
+    styleUrls: ['./video-embed.component.scss']
 })
 export class VideoEmbedComponent implements OnInit {
 
-    @Input() site: string = "Youtube";
+    @Input() site: string = "";
     @Input() key: string | null = null;
+
+
+    videoUrl: SafeResourceUrl = "";
 
     constructor(private sanitazer: DomSanitizer) { }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {
+        switch (this.site) {
+            case "YouTube":
+                this.videoUrl = this.getSafeUrl("https://www.youtube.com/embed/" + this.key);
+                break
+            case "Vimeo":
+                this.videoUrl = this.getSafeUrl("https://player.vimeo.com/video/" + this.key);
+                break
+        }
+    }
 
-    getSafeUrl(url: string)
-    {
+    getSafeUrl(url: string) {
         return this.sanitazer.bypassSecurityTrustResourceUrl(url);
     }
 
